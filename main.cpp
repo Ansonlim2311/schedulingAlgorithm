48c5c7681da3f9c1ef6a8e11ce3773468277f9dc
Ansonlim2311 <ansonlim2311@gmail.com>
2024-12-23 10:26:29 +0800

    update the reqirement from pdf

--- main.cpp
+++ main.cpp
@@ -3,19 +3,59 @@
 
 using namespace std;
 
-void roundRobin() {
+void roundRobin(int arrivalTime[], int burstTime[], int waitingTime[], int turnaroundTime[], int priority[], int remainingBurstTime[], int numProcesses) {
+    float averageWaitingTime = 0, averageTurnaroundTime = 0;
+
+    // for checking purpose
+    // cout << "Arrival times for each process:\n";
+    // for (int i = 0; i < numProcesses; i++) {
+    //     cout << "Process P" << i + 1 << ": ";
+    //     cout << arrivalTime[i];
+    //     cout << endl;
+    // }
+
+    // cout << "Burst times for each process:\n";
+    // for (int i = 0; i < numProcesses; i++) {
+    //     cout << "Process P" << i + 1 << ": ";
+    //     cout << burstTime[i];
+    //     remainingBurstTime[i] = burstTime[i];
+    //     cout << endl;
+    // }
+
+    // cout << "Priority for each process:\n";
+    // for (int i = 0; i < numProcesses; i++) {
+    //     cout << "Process P" << i + 1 << ": ";
+    //     cout << priority[i];
+    //     cout << endl;
+    // }
+}
+
+void SRT() {
+
+}
+
+void preemptivePriority() {
+
+}
+
+void non_preemptivePriority() {
+
+}
+
+int main() {
+
     int numProcesses;
+
     cout << "Enter the number of processes: ";
     cin >> numProcesses;
 
     if (numProcesses < 3 || numProcesses > 10) {
         cout << "Number of processes must be between 3 and 10." << endl;
-        return;
+        return 0;
     }
 
     int arrivalTime[numProcesses], burstTime[numProcesses], waitingTime[numProcesses], turnaroundTime[numProcesses], priority[numProcesses];
     int remainingBurstTime[numProcesses];
-    float averageWaitingTime = 0, averageTurnaroundTime = 0;
 
     cout << "Enter arrival times for each process:\n";
     for (int i = 0; i < numProcesses; i++) {
@@ -35,42 +75,26 @@ void roundRobin() {
         cout << "Process P" << i + 1 << ": ";
         cin >> priority[i];
     }
-}
 
-void SRT() {
-
-}
-
-void preemptivePriority() {
-
-}
-
-void non_preemptivePriority() {
-
-}
-
-int main() {
+    roundRobin(arrivalTime, burstTime, waitingTime, turnaroundTime, priority, remainingBurstTime, numProcesses);
+    
     int choice;
 
     cout << "Choose scheduling algorithm:\n";
-    cout << "1. Round Robin Of Quantum 3\n";
-    cout << "2. Shortest Remaining Time\n";
-    cout << "3. Preemptive Priority\n";
-    cout << "4. Non-Preemptive Priority\n";
+    cout << "1. Shortest Remaining Time\n";
+    cout << "2. Preemptive Priority\n";
+    cout << "3. Non-Preemptive Priority\n";
     cout << "Enter your choice: ";
     cin >> choice;
 
     switch (choice) {
         case 1:
-            roundRobin();
-            break;
-        case 2:
             SRT();
             break;
-        case 3:
+        case 2:
             preemptivePriority();
             break;
-        case 4:
+        case 3:
             non_preemptivePriority();
             break;
         default:
